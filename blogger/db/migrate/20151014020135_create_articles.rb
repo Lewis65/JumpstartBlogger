37ca11c51d116149d28817963bc7c6ff50e82469
Lewis Horwood <horwood.lewis@gmail.com>
2015-11-01 18:13:06 -0700

    Added tagging and a few bells and whistles

--- blogger/db/migrate/20151014020135_create_articles.rb
+++ blogger/db/migrate/20151014020135_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.text :body
+      t.text :tag_list
       t.timestamps
     end
   end
